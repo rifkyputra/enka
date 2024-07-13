48bba2fc550c775696b75431324fabf9d9e5a2bd
Rifky A. Putra <rifqempul@gmail.com>
2024-07-13 17:33:29 +0700

    Initial commit

---
+++ www/dash/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LAMP STACK</title>
+        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
+        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    </head>
+    <body>
+        <section class="hero is-medium is-info is-bold">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        LAMP STACK
+                    </h1>
+                    <h2 class="subtitle">
+                        Your local development environment
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered">Environment</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><?= apache_get_version(); ?></li>
+                                <li>PHP <?= phpversion(); ?></li>
+                                <li>
+                                    <?php
+                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+
+                                    /* check connection */
+                                    if (mysqli_connect_errno()) {
+                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
+                                    } else {
+                                        /* print server version */
+                                        printf("MySQL Server %s", mysqli_get_server_info($link));
+                                    }
+                                    /* close connection */
+                                    mysqli_close($link);
+                                    ?>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><a href="/phpinfo.php">phpinfo()</a></li>
+                                <li><a href="//<? print $_SERVER['HTTP_HOST']; ?>:<? (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? print $_ENV['PMA_SECURE_PORT'] : print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
+                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
+                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong><a href="https://robertsaupe.de" target="_blank">Robert Saupe</a></strong><br>
+                    <br>
+                    <a href="https://github.com/robertsaupe/docker-compose-lamp" target="_blank">github.com/robertsaupe/docker-compose-lamp</a><br>
+                    The source code is released under the <a href="https://github.com/robertsaupe/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT license</a>.<br>
+                    <br>
+                    The project is a fork of <a href="https://github.com/sprintcube/docker-compose-lamp" target="_blank">github.com/sprintcube/docker-compose-lamp</a>.<br>
+                </p>
+            </div>
+        </footer>
+    </body>
+</html>
